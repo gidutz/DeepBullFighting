86c2cb90bdcef4b0a4942a94b40ba2aa762d12a4
gad <gidutz@gmail.com>
2019-11-05 19:32:55 +0200

    changed hostname to parameter

--- gym/deep_racer_env.py
+++ gym/deep_racer_env.py
@@ -6,16 +6,16 @@ class DeepRacerEnv():
     START = 'start'
     STOP = 'stop'
 
-    def __init__(self, x_csrf_token, cookie):
+    def __init__(self, x_csrf_token, cookie, host):
         self.x_csrf_token = x_csrf_token
         self.cookie = cookie
-        self.hostname = 'https://172.20.1.54/'
+        self.hostname = host
 
     def get_headers(self):
         headers = {
-            'authority': '172.20.1.54',
+            'authority': self.hostname,
             'x-requested-with': 'XMLHttpRequest',
-            'origin': 'https://172.20.1.54',
+            'origin': 'https://{}'.format(self.host),
             'x-csrf-token': self.x_csrf_token,
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36',
             'content-type': 'application/json;charset=UTF-8',
@@ -45,10 +45,10 @@ class DeepRacerEnv():
         return response
 
     def start_riding(self):
-        return self.start_stop(DeepRacerAgent.START)
+        return self.start_stop(DeepRacerEnv.START)
 
     def stop_riding(self):
-        return self.start_stop(DeepRacerAgent.STOP)
+        return self.start_stop(DeepRacerEnv.STOP)
 
     def reset(self):
         self.stop_riding()
