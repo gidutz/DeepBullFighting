934b89bdcf7b95ded2e2d33fa1e5eba08a121c6c
malbinir <nirm@gett.com>
2019-11-07 23:44:25 +0200

    dqn

--- gym/agents.py
+++ gym/agents.py
@@ -6,7 +6,6 @@ from keras.models import Model
 from deep_racer_env import DeepRacerEnv
 
 
-
 class DQNAgent:
     def __init__(self, env: DeepRacerEnv, gamma, exploraion_rate):
         self.env = env
