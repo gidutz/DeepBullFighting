3ca741bb904ae5971c0add5e40646916d1583286
gad <gidutz@gmail.com>
2019-11-18 18:26:08 +0200

    improved game

--- gym/game.py
+++ gym/game.py
@@ -1,14 +1,17 @@
+import warnings
+warnings.filterwarnings("ignore")
 
+import numpy as np
+from datetime import datetime
 from deep_racer_env import DeepRacerEnv
 from gym_wrappers import ObservationToDetectionWrapper, DeepRewardWrapper, ContinuesToDiscreteActionWrapper
 from agents import DQNAgent
 from os.path import expanduser
-import warnings
-warnings.filterwarnings("ignore")
+DATE_FORMAT = '%Y%m%d-%M%S'
 
 if __name__ == '__main__':
-    x_csrf_token = 'IjJkNjU0NzIwOWZmMDVmZGUzNTExYzQ3MjFmNWFlMzczYmVkYTRhMjEi.Xc1IOg.RUyaPAms-Ka6WTBIEJiv6xNA4-4'
-    cookie = 'session=eyJjc3JmX3Rva2VuIjp7IiBiIjoiTW1RMk5UUTNNakE1Wm1Zd05XWmtaVE0xTVRGak5EY3lNV1kxWVdVek56TmlaV1JoTkdFeU1RPT0ifX0.Xc0dnA.ZZ2GmUyBYBlKC6Pv2_ny1a-yZUw; deepracer_token=0e256035-578b-4245-ab71-59c0d9e986b1'
+    x_csrf_token = 'IjRjOTcwOGM5ZTFjOGJjMmQ4NTUwYTVlYWYyYWU3NWE0Y2MwZDRmMTQi.XdK-Dg.-6lcHQDEbaRs3HtEbhSbwAAwuEQ'
+    cookie = 'session=eyJjc3JmX3Rva2VuIjp7IiBiIjoiTkdNNU56QTRZemxsTVdNNFltTXlaRGcxTlRCaE5XVmhaakpoWlRjMVlUUmpZekJrTkdZeE5BPT0ifX0.XdK9_g.Uy9HA2sMiYKp1HVK182xL1OvIMw; deepracer_token=1b0fcc24-d89d-47d1-a25e-b3ca391acb1b'
     host = '172.20.1.54'
     model_data_dir = expanduser('~/PycharmProjects/DeepBullFighter/gym/object_detection')
     racer_env = DeepRacerEnv(x_csrf_token=x_csrf_token, cookie=cookie, host=host, model_data_dir=model_data_dir)
@@ -20,14 +23,20 @@ if __name__ == '__main__':
 
     done = False
     obs = racer_env.reset()
-
+    game_ts = datetime.utcnow().strftime(DATE_FORMAT)
     for episode in  range (100):
         print('Playing episode:', episode)
+        rewards = []
         while not done:
             action = agent.act(obs)
             print(obs, action, racer_env.action(action))
             obs_next, reward, done, _ = racer_env.step(action)
             agent.feedback(obs, action, reward, obs_next)
             obs = obs_next
+            rewards.append(reward)
         racer_env.reset()
-
+        done = False
+        print("Total Reward: ", np.sum(rewards))
+        with open('./agents_data/{}.txt'.format(game_ts), 'w+') as file:
+            file.write(str(rewards))
+            file.write("\n")
